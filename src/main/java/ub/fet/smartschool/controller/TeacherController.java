478abd4f5965fe16b738645548a155dcdb0e83fe
lobeserge <46405853+lobeserge@users.noreply.github.com>
2020-07-15 17:30:28 +0100

    update

--- src/main/java/ub/fet/smartschool/controller/TeacherController.java
+++ src/main/java/ub/fet/smartschool/controller/TeacherController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/api/teacher")
 public class TeacherController {
