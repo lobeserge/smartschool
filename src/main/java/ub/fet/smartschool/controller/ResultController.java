2cc67a4983f0a3d984bd4c16a17b4acdd41ff179
lobeserge <46405853+lobeserge@users.noreply.github.com>
2020-07-15 15:54:44 +0100

    CORS

--- src/main/java/ub/fet/smartschool/controller/ResultController.java
+++ src/main/java/ub/fet/smartschool/controller/ResultController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", allowedHeaders = "*" maxAge = 3600)
 @RestController
 @RequestMapping("/api/result")
 public class ResultController {
